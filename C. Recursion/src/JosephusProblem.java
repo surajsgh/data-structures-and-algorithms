c99c1b8a945a931c607cf333331765a5ccb4bf0a
Suraj Singh <surajsingh220698@outlook.com>
2022-02-21 11:29:49 +0530

    Add auxiliary space for the following program.

--- C. Recursion/src/JosephusProblem.java
+++ C. Recursion/src/JosephusProblem.java
@@ -5,6 +5,7 @@ public class JosephusProblem {
 
     //  This is applicable only if the number starts from 0, not one.
     //  Time Complexity : O(n)
+    //  Auxiliary Space : O(n)
     public static int josephusProblem(int n, int k) {
         if (n==1) {
             return 0;
