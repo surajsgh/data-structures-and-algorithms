ba38cbf7fbbf8e3b04c809c6a295b74343338279
Suraj Singh <surajsingh220698@outlook.com>
2022-03-07 17:02:02 +0530

    Add generate permutations of given string best solution.

--- S. Backtracking/src/GeneratePermutationsOfString.java
+++ S. Backtracking/src/GeneratePermutationsOfString.java
@@ -1,28 +1,35 @@
 public class GeneratePermutationsOfString {
     public static void main(String[] args) {
         String str = "ABC";
-        permutation(str.toCharArray(), 0, str.length());
+        permutation(str.toCharArray(), 0, str.length()-1);
+    }
+
+    private static boolean isSafe(char[] characters, int low, int i, int high) {
+        if (low!=0 && characters[low-1]=='A' && characters[i]=='B') return false;
+        if (high==(low+1) && characters[i]=='A' && characters[low]=='B') return false;
+        return true;
     }
 
     //  Naive Solution
-    private static void permutation(char[] characters, int low, int right) {
-        if (low==right) {
-            if (!String.valueOf(characters).contains("AB")) {
-                System.out.println(String.valueOf(characters));
-            }
+    private static void permutation(char[] characters, int low, int high) {
+        if (low==high) {
+            System.out.println(String.valueOf(characters));
             return;
         }
+        else {
+            for (int i=low; i<=high; i++) {
+                if (isSafe(characters, low, i, high)) {
+                    char temp = characters[low];
+                    characters[low] = characters[i];
+                    characters[i] = temp;
 
-        for (int i=low; i<right; i++) {
-            char temp = characters[low];
-            characters[low] = characters[i];
-            characters[i] = temp;
+                    permutation(characters, low+1, high);
 
-            permutation(characters, low+1, right);
-
-            temp = characters[low];
-            characters[low] = characters[i];
-            characters[i] = temp;
+                    temp = characters[low];
+                    characters[low] = characters[i];
+                    characters[i] = temp;
+                }
+            }
         }
     }
 }
