9ea78bb46ff6494a5954833c25b0ff8d411abfa1
Suraj Singh <surajsingh220698@outlook.com>
2022-05-21 21:26:57 +0530

    Add naive solution for product array result problem

--- D. Array/src/ProductArrayResult.java
+++ D. Array/src/ProductArrayResult.java
@@ -1,7 +1,7 @@
 public class ProductArrayResult {
     //  Time Complexity : O(n)
     //  Auxiliary Space : O(1)
-    public int[] productExceptSelf(int[] nums) {
+    public int[] productExceptSelfEff(int[] nums) {
         int n = nums.length;
         int[] result = new int[n];
         for(int i=0, temp=1; i<n; i++) {
@@ -12,6 +12,33 @@ public class ProductArrayResult {
             result[i] *= temp;
             temp *= nums[i];
         }
+        return result;
+    }
+
+    //  Time Complexity : O(n)
+    //  Auxiliary Space : O(n)
+    public int[] productExceptSelfNaive(int[] nums) {
+        int n = nums.length;
+
+        int[] leftArray = new int[n];
+        int[] rightArray = new int[n];
+        int[] result = new int[n];
+
+        leftArray[0] = 1;
+        rightArray[n-1] = 1;
+
+        for(int i=1; i<n; i++) {
+            leftArray[i] = leftArray[i-1]*nums[i-1];
+        }
+
+        for(int i=n-2; i>=0; i--) {
+            rightArray[i] = rightArray[i+1]*nums[i+1];
+        }
+
+        for(int i=0; i<n; i++) {
+            result[i] = leftArray[i] * rightArray[i];
+        }
+
         return result;
     }
 }
