162b9749f00a3a99373f5e84fcb71de6b4d53cf9
Suraj Singh <surajsingh220698@outlook.com>
2022-06-25 13:59:21 +0530

    Modify find the connected components program.

--- Q. Graphs/src/ForFindingConnectedComponents.java
+++ Q. Graphs/src/ForFindingConnectedComponents.java
@@ -3,26 +3,26 @@ import java.util.ArrayList;
 
 public class ForFindingConnectedComponents {
     //  Number of islands problem using DFS
-    public static void dfs(ArrayList<ArrayList<Integer>> list, int v, int s) {
+    public static int dfs(ArrayList<ArrayList<Integer>> list, int v, int s) {
+        int count = 0;
         boolean[] visited = new boolean[v+1];
         for (int i=0; i<v; i++) {
             if (visited[i]==false) {
                 dfsRec(list, i, visited);
+                count++;
             }
         }
+        return count;
     }
 
     //  Time Complexity : O(V+E)
-    public static int dfsRec(ArrayList<ArrayList<Integer>> list, int s, boolean[] visited) {
-        int count = 0;
+    public static void dfsRec(ArrayList<ArrayList<Integer>> list, int s, boolean[] visited) {
         visited[s] = true;
         System.out.print(s + " ");
         for (int num : list.get(s)) {
             if (visited[num]==false) {
                 dfsRec(list, num, visited);
-                count++;
             }
         }
-        return count;
     }
 }
