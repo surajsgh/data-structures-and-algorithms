cf16211de355bc5abb902691935f5530936e7b49
Suraj Singh <surajsingh220698@outlook.com>
2022-07-02 17:54:12 +0530

    Add find nth fibonacci problem solution.

---
+++ T. Dynamic Programming/src/NthFibonacciNumber.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class NthFibonacciNumber {
+    public static int num = 3;
+    public static int[] memo = new int[num+1];
+
+    public static void main(String[] args) {
+        Arrays.fill(memo, -1);
+        System.out.println(findNthFibonacciNumberNaive(num));
+        System.out.println(findNthFibonacciEff(num));
+    }
+
+    //  Memoization Technique
+    //  Time Complexity : O(n)
+    public static int findNthFibonacciEff(int n) {
+        if (memo[n]==-1) {
+            int res;
+            if (n<=1) {
+                res = n;
+            }
+            else {
+                res = findNthFibonacciEff(n-1) + findNthFibonacciEff(n-2);
+            }
+            memo[n] = res;
+        }
+        return memo[n];
+    }
+
+    //  Time Complexity : O(2**n)
+    public static int findNthFibonacciNumberNaive(int n) {
+        if (n<=1) {
+            return n;
+        }
+        return findNthFibonacciNumberNaive(n-1) + findNthFibonacciNumberNaive(n-2);
+    }
+}
