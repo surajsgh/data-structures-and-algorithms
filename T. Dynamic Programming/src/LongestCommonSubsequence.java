13be339a18f329c1f62a257d5d9a65d59065ebbc
Suraj Singh <surajsingh220698@outlook.com>
2022-07-10 11:43:12 +0530

    Modify longest common subsequence problem.

--- T. Dynamic Programming/src/LongestCommonSubsequence.java
+++ T. Dynamic Programming/src/LongestCommonSubsequence.java
@@ -39,7 +39,7 @@ public class LongestCommonSubsequence {
                     matrix[i][j] = 1 + matrix[i-1][j-1];
                 }
                 else {
-                    matrix[i][j] = matrix[i-1][j] + matrix[i][j-1];
+                    matrix[i][j] = Math.max(matrix[i-1][j], matrix[i][j-1]);
                 }
             }
         }
