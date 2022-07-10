f5f52d6882682670258580bd124ad56c05d5377f
Suraj Singh <surajsingh220698@outlook.com>
2022-07-10 13:24:25 +0530

    Add edit distance problem.

--- T. Dynamic Programming/src/EditDistanceProblem.java
+++ T. Dynamic Programming/src/EditDistanceProblem.java
@@ -1,6 +1,7 @@
 public class EditDistanceProblem {
     public static void main(String[] args) {
         System.out.println(editDistanceNaive("CAT", "CUT", 3, 3));
+        System.out.println(editDistance("CAT", "CUT", 3, 3));
     }
 
     //  Time Complexity : O(3^m)
@@ -11,5 +12,32 @@ public class EditDistanceProblem {
             return editDistanceNaive(first, second, m-1, n-1);
         }
         return 1 + Math.min(editDistanceNaive(first, second, m-1, n), Math.min(editDistanceNaive(first, second, m, n-1), editDistanceNaive(first, second, m-1, n-1)));
+    }
+
+    //  Time Complexity : O(mn)
+    //  Auxiliary Space : O(mn)
+    public static int editDistance(String first, String second, int m, int n) {
+        int[][] matrix = new int[m+1][n+1];
+
+        for (int i=0; i<m+1; i++) {
+            matrix[i][0] = i;
+        }
+
+        for (int i=0; i<n+1; i++) {
+            matrix[0][i] = i;
+        }
+
+        for (int i=1; i<m+1; i++) {
+            for (int j=1; j<n+1; j++) {
+                if (first.charAt(i-1)==second.charAt(j-1)) {
+                    matrix[i][j] = matrix[i-1][j-1];
+                }
+                else {
+                    matrix[i][j] = 1 + Math.min(matrix[i-1][j-1], Math.min(matrix[i-1][j], matrix[i][j-1]));
+                }
+            }
+        }
+
+        return matrix[m-1][n-1];
     }
 }
