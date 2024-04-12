3c417b8ee9dc0da1ed6a184c185b8219e5488ca0
surajsgh <surajsingh220698@outlook.com>
2024-04-12 18:24:49 +0530

    Add an optimized approach for KnapSackProblem

--- T. Dynamic Programming/src/KnapSackProblem.java
+++ T. Dynamic Programming/src/KnapSackProblem.java
@@ -5,8 +5,38 @@ public class KnapSackProblem {
         int weight = 10;
         int n = 4;
         System.out.println(knapSackProblem(weights, arr, weight, n));
+        System.out.println(knapSackProblemDP(weights, arr, weight, n));
     }
 
+    //  Time Complexity: O(w*n)
+    //  Auxiliary Space: O(w*n)
+    public static int knapSackProblemDP(int[] weights, int[] values, int weight, int n) {
+        int[][] matrix = new int[n+1][weight+1];
+
+        for (int i=0; i<=weight; i++) {
+            matrix[0][i] = 0;
+        }
+
+        for(int i=0; i<=n; i++) {
+            matrix[i][0] = 0;
+        }
+
+        for (int i=1; i<n+1; i++) {
+            for (int j=1; j<weight+1; j++) {
+                if(weights[i-1]>j) {
+                    matrix[i][j] = matrix[i-1][j];
+                }
+                else {
+                    matrix[i][j] = Math.max(matrix[i-1][j], matrix[i-1][j-weights[i-1]] + values[i-1]);
+                }
+            }
+        }
+
+        return matrix[n][weight];
+    }
+
+    //  Time Complexity: O(2**N)
+    //  Auxiliary Space: O(2**N)
     public static int knapSackProblem(int[] weights, int[] values, int weight, int n) {
         if (n==0 || weight==0) return 0;
         if (weights[n-1]>weight) {
